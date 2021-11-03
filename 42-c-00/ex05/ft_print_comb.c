d4461fe410b24bf928cc41b8d144193492cab55d
lerodrig <lerodrig@student.42barcelona.com>
2021-11-03 13:34:04 +0100

    asffdsfasd

--- 42-c-00/ex05/ft_print_comb.c
+++ 42-c-00/ex05/ft_print_comb.c
@@ -3,7 +3,37 @@
 void	ft_print_comb(void)
 {
 	// char o1[1] = "0";
-	char o2[2] = "00";
+	// char o2[2] = "00";
 
-	write(1, &o2, sizeof(o2));	
+	// write(1, &o2, sizeof(o2));	
+	
+	// int a, b, c;
+	// a = b = c = '0';
+
+	int a = 0;
+	int b = 1;
+
+	char x = '0';
+	char y = '1';
+
+	char s[2] = ", ";
+
+	while (a < 10){
+		// write(1, &x, 1);
+		
+		while(b < 10){
+			write(1, &x, 1);
+			write(1, &y, 1);
+			
+			b++;
+			y++;
+
+			write(1, s, 2);
+		}
+
+		write(1, s, 2);
+
+		a++;
+		x++;
+	}
 }
